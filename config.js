9588028d92d48ef02e8045ff58480d465a52fbfa
Djouaher Rachid <152193959+DjouaherRachid@users.noreply.github.com>
2024-04-02 13:13:11 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   s3: {
-    accessKeyId: '', // TODO
-    secretAccessKey: '', // TODO
-    endpoint: '', // TODO
-    bucket: '', // TODO
+    accessKeyId: 'YDNAK33N6215AU57QJVL', // TODO
+    secretAccessKey: 'jccCK27YyQep53tevstYjrW6dOSGbbIezff90vZF', // TODO
+    endpoint: 's3.eu-west-2.wasabisys.com', // TODO
+    bucket: 'esgi-cloud-exam-bucket-85213', // TODO
   },
   suffix: {
     small: '_small.jpg',
